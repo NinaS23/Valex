d0c63c0444307a7f543d22ce1cef437fe1e6c82b
MariaDuda <mariabevenuto062@gmail.com>
2022-08-30 12:52:29 -0300

    fix files places

--- src/repositories/cardRepository.ts
+++ src/repositories/cardRepository.ts
@@ -1,5 +1,5 @@
-import { connection } from "../database.js";
-import { mapObjectToUpdateQuery } from "../utils/sqlUtils.js";
+import { connection } from "../config/database";
+import { mapObjectToUpdateQuery } from "../utils/sqlUtils";
 
 export type TransactionTypes =
   | "groceries"
