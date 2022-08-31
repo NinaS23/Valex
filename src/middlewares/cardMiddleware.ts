d68bceb79df114a862ed6272211f8703d1517811
MariaDuda <mariabevenuto062@gmail.com>
2022-08-31 02:54:02 -0300

    feat employerid at middleware

--- src/middlewares/cardMiddleware.ts
+++ src/middlewares/cardMiddleware.ts
@@ -3,9 +3,10 @@ import { cardSchema } from "../schemas/cardSchema.js";
 
 export async function validateCardInfo(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.header('x-api-key');
-    
-    const { error } = cardSchema.validate({apiKey}, {abortEarly: false});
-
+    const {employeeId} : {employeeId:number} = req.body;
+   
+    const { error } = cardSchema.validate({apiKey,employeeId}, {abortEarly: false});
+   
     if (error) {
         return res.status(422).send(error.details.map(detail => detail.message));
     }
--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -2,4 +2,5 @@ import joi from "joi";
 
 export const cardSchema = joi.object({
     apiKey: joi.string().required(),
+    employeeId: joi.number().required()
 });
