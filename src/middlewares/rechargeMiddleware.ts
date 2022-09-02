a4e54d42ee6b1e0015feec02c45d0c2ffbf831b6
MariaDuda <mariabevenuto062@gmail.com>
2022-09-01 23:37:02 -0300

    add validation on rechargemiddleware

--- src/middlewares/rechargeMiddleware.ts
+++ src/middlewares/rechargeMiddleware.ts
@@ -5,9 +5,11 @@ export async function validateRechargeInfo(req: Request, res: Response, next: Ne
     const {amount} : {amount : number} = req.body
     const apiKey = req.header('x-api-key');
     const cardId = req.params.id;
-
+    
+    if(amount  === 0){
+        return res.status(401).send("0 não é permitido");
+    }
     const { error } = rechargeSchema.validate({apiKey, cardId, amount}, {abortEarly: false});
-
     if (error) {
         return res.status(422).send(error.details.map(detail => detail.message));
     }
--- src/services/rechargeService.ts
+++ src/services/rechargeService.ts
@@ -1,4 +1,13 @@
+import * as companyRepository from "../repositories/companyRepository.js";
+import * as cardRepository from "../repositories/cardRepository.js";
 
 export async function rechargeCard(cardId: number, apiKey:string, amount:number) {
- 
+    const isApiKeyValid = await companyRepository.findByApiKey(apiKey)
+    if (!isApiKeyValid) {
+        throw { code: "not-found", message: "api-key is not Found" }
+    }
+    const card = await cardRepository.findById(cardId);
+    if (!card) {
+        throw { code: "not-found", message: "card was not found" }
+    }
 }
