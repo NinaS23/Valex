edc2aaa756cc6b1ae5bc1ad3eb0805fab1567843
Maria schwarz <98234978+NinaS23@users.noreply.github.com>
2022-09-04 22:16:14 -0300

    Update cardService.ts

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -97,7 +97,7 @@ export async function viewTransectionAndBalance(cardId: number) {
     const shopping = await paymentRepository.findByCardId(cardId);
     const recharges = await rechargeRepository.findByCardId(cardId);
 
-    if (shopping.length === 0 || recharges.length === 0) {
+    if (shopping.length === 0 && recharges.length === 0) {
         throw { code: "no-content", message: "no recharges or transections found" }
     }
     const balance = await cardUtils.calcBalance(recharges, shopping)
